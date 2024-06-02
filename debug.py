68bc02fc6636d9ef8048cbf7b1439f41726a235b
jopurt <doronin.stepa@mail.ru>
2024-06-02 17:25:01 +0500

    Create debug.py

---
+++ debug.py
@@ -0,0 +1,11 @@
+if MODE == "INITIALIZATION":
+    pass
+
+elif MODE == "RUNNING":
+    data = INPUT["Data"]
+    print("________________NEW___________________________________________________")
+    print(data)
+    print("=====================================================================\n")
+
+elif MODE == "DESTRUCTION":
+    pass
